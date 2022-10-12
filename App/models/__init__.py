9696c4f87809fdd5c25eb8a6e9b86edcd954e63f
Titangrass <ajaygunness98@gmail.com>
2022-10-12 05:02:55 +0000

    modified:   App/models/__init__.py 	modified:   App/models/review.py

--- App/models/__init__.py
+++ App/models/__init__.py
@@ -1,1 +1,5 @@
 from .user import *
+from .admin import *
+from .review import *
+from .staff import *
+from .student import *
--- App/models/review.py
+++ App/models/review.py
@@ -7,17 +7,14 @@ class Review(db.Model):
     comment = db.Column(db.String)
     like = db.Column(db.Integer, nullable=False)
     dislike = db.Column(db.Integer, nullable=False)
-    
-    
-    def __init__(self, student_firstName, student_lastName):
-        self.student_firstName = student_firstName
-        self.student_lastName = student_lastName
+        
+    def __init__(self, reviewId):
+        self.reviewId = reviewId
     
 
     def toJSON(self):
         return{
-            'student_firstName' = student_firstName
-            'student_lastName' = student_lastName
+            'reviewId' = reviewId
         }
 
 
